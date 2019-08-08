6f313c68b6e1b0d956d4033e01e290ba4c2b97cc
Tiffany Tong <Tiffany@LP-JQ2ZK12.GEO.CORP.HCL.IN>
2019-08-08 13:17:25 -0400

    Section 2 complete

---
+++ ppmtool/src/main/java/com/hcltech/ppmtool/repositories/ProjectRepository.java
@@ -0,0 +1,17 @@
+package com.hcltech.ppmtool.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import com.hcltech.ppmtool.domain.Project;
+
+@Repository
+public interface ProjectRepository extends CrudRepository<Project, Long>{
+
+	Project findByProjectIdentifier(String projectId);
+
+	@Override
+	Iterable<Project> findAll();
+		
+	
+} // interface
